2db0162ecc4ff0f79adafa12f1c007bcf59c3d8a
Juho Suni <juho.suni@protonmail.com>
2023-05-15 14:07:29 +0300

    Initial commit for boilerplate

---
+++ src/main/java/com/js/portfolio_backend/App.java
@@ -0,0 +1,18 @@
+package com.js.portfolio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class App {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(App.class, args);
+		RestTemplate restTemplate = (RestTemplate) ctx.getBean("protoRestTemplate");
+		System.out.println(restTemplate);
+	}
+
+}
---
+++ src/test/java/com/js/portfolio_backend/AppTests.java
@@ -0,0 +1,13 @@
+package com.js.portfolio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
